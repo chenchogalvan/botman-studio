66d72efc8518584b1606c0672b79a6ddbbc654d7
Alfredo Galvan <alfredogalvan.91@gmail.com>
2023-11-30 00:12:24 -0600

    facebook login

--- resources/views/facebook.blade.php
+++ resources/views/facebook.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
 </head>
 <body>
+<!--
 <script>
 
     function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
@@ -50,15 +51,44 @@
     }
 
 </script>
-
+-->
 
 <!-- The JS SDK Login Button -->
 
-<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+{{--<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">--}}
+{{--</fb:login-button>--}}
+
+{{--<div id="status">--}}
+{{--</div>--}}
+
+
+
+<fb:login-button
+        scope="public_profile,email"
+        onlogin="checkLoginState();">
 </fb:login-button>
 
-<div id="status">
-</div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '10160615857336564',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v18.0'
+        });
+
+        FB.AppEvents.logPageView();
+
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <!-- Load the JS SDK asynchronously -->
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
