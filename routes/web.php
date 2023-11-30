7d4ffcbd27c00e7297094fe71e6ad7465cea8b29
Alfredo Galvan <alfredogalvan.91@gmail.com>
2023-11-30 00:47:16 -0600

    facebook login

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('facebook');
+    return view('welcome');
 });
 
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
 
-Route::get('/facebook', function() {
-    return view('facebook');
-});
+//Route::get('/facebook', function() {
+//    return view('facebook');
+//});
